f0e00981df982027fe6d219b153eae9db2e334fc
Logan Lahlah <logan.lahlah@etu.u-bordeaux.fr>
2025-10-16 15:27:47 +0200

    rectification

--- projetHPC/mini-chameleon/publish.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'load-path "./include/compose-publish/")
-(require 'compose-publish)
-
-(setq org-html-link-home "https://solverstack.gitlabpages.inria.fr/mini-examples/mini-chameleon/")
-(setq org-html-link-up "../index.html")
-
